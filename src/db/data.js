57b43d5e9d5fc805bd0dfc6d861fbf15db01666d
Akos Oravecz <oraveczakos2001@gmail.com>
2022-09-24 13:34:14 +0200

    create data.js - store ratio between units

---
+++ src/db/data.js
@@ -0,0 +1,9 @@
+[
+    {"név": "milligramm", "rövidítés": "mg", "arány": 0.001},
+    {"név": "centigramm", "rövidítés": "cg", "arány": 0.01},
+    {"név": "decigramm", "rövidítés": "dg", "arány": 0.1},
+    {"név": "gramm", "rövidítés": "g", "arány": 1},
+    {"név": "dekagramm", "rövidítés": "dkg", "arány": 10},
+    {"név": "kilogramm", "rövidítés": "kg", "arány": 1000},
+    {"név": "tonna", "rövidítés": "t", "arány": 1000000}
+]
